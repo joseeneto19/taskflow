faea6d1bf5b2857f9eef1f1e2e79aa56c1370b88
joseeneto19 <josemnetodev@gmail.com>
2026-03-20 16:07:20 -0300

    feat: create basic crud

--- backend/src/main/java/com/taskflow/controllers/TaskController.java
+++ backend/src/main/java/com/taskflow/controllers/TaskController.java
@@ -1,9 +1,45 @@
 package com.taskflow.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.taskflow.entities.Task;
+import com.taskflow.servicies.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TaskController {
+
+    private final TaskService service;
+
+    public TaskController(TaskService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Task create(@RequestBody Task task) {
+        return service.create(task);
+    }
+
+    @GetMapping
+    public List<Task> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> findById(@PathVariable UUID id) {
+        return service.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task update(@PathVariable UUID id, @RequestBody Task task) {
+        return service.update(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id) {
+        service.delete(id);
+    }
 }
--- backend/src/main/java/com/taskflow/servicies/TaskService.java
+++ backend/src/main/java/com/taskflow/servicies/TaskService.java
@@ -1,8 +1,44 @@
 package com.taskflow.servicies;
 
+import com.taskflow.entities.Task;
+import com.taskflow.repositories.TaskRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.yaml.snakeyaml.events.Event;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TaskService {
 
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public Task create(Task task) {
+        return repository.save(task);
+    }
+
+    public List<Task> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Task> findById(UUID id) {
+        return repository.findById(id);
+    }
+
+    public Task update(UUID id, Task task) {
+        Task findTask = repository.getReferenceById(id);
+        BeanUtils.copyProperties(task, findTask, "id");
+        return repository.save(findTask);
+    }
+
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
 }
